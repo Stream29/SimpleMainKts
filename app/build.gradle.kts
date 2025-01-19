77165b93908a03d8b9dd77e6bfad1e88cfbc249f
Stream <1542763342@qq.com>
2025-01-19 10:26:45 +0800

    feat: copied simple implementation from https://github.com/Kotlin/kotlin-script-examples

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,10 @@ plugins {
 
 dependencies {
     // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
-    implementation(project(":utils"))
+    implementation(libs.bundles.kotlinx.all)
+    implementation(libs.bundles.kotlin.scripting)
+    implementation(libs.apache.ivy)
+
 }
 
 application {
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     id("buildsrc.convention.kotlin-jvm")
     // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
-    alias(libs.plugins.kotlinPluginSerialization)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
-    implementation(libs.bundles.kotlinxEcosystem)
+    implementation(libs.bundles.kotlinx.all)
     testImplementation(kotlin("test"))
 }
