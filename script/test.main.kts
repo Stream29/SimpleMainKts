b9145dc0c228a6aa4d1e4048a0be85311bee7716
Stream <1542763342@qq.com>
2025-01-20 01:04:12 +0800

    feat: find out that KMP dependency should have a "-jvm" suffix

--- script/test.main.kts
+++ script/test.main.kts
@@ -1,9 +1,14 @@
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+@file:DependsOn("io.github.stream29:langchain4kt-core-jvm:1.0.0")
 
+import io.github.stream29.langchain4kt.core.message.Message
+import io.github.stream29.langchain4kt.core.message.MessageSender
 import kotlinx.html.body
 import kotlinx.html.h1
 import kotlinx.html.html
 import kotlinx.html.stream.createHTML
+
+println(Message(MessageSender.User, "hello, model"))
 
 val addressee = args.firstOrNull() ?: "World"
 
